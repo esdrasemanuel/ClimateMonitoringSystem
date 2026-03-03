0aa76a2ca92d34ff056196cd841ac505c94f0200
esdrasemanuel <emanuelmariano.m@gmail.com>
2026-03-03 22:35:45 +0000

    start

---
+++ src/main/java/com/esdras/climatemonitoringsystem/ClimateMonitoringSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.esdras.climatemonitoringsystem;
+
+/**
+ *
+ * @author EMoreira
+ */
+public class ClimateMonitoringSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
